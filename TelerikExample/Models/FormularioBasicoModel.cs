ab8b3c938e2d741cec3ad2e2ab2cbed4a9aab9da
Jorge Polanco Peña <jpolanco@cic.es>
2017-11-10 10:56:32 +0100

    Nuevos cambios

--- TelerikExample/Models/FormularioBasicoModel.cs
+++ TelerikExample/Models/FormularioBasicoModel.cs
@@ -15,6 +15,28 @@ namespace TelerikExample.Models
         [EmailAddress]
         [Display(Name = "Email del usuario")]
         public string email { get; set; }
+        [Required]
+        [Display(Name = "Precio maximo")]
+        public decimal precioMaximo { get; set; }
+
+        [Required]
+        [Display(Name = "Fecha de estreno")]
+        public DateTime fecha { get; set; }
+
+        [Display(Name = "Personas a invitar")]
+        public List<string> listNombresSelected { get; set; }
+        public List<string> listNombres { get; set; }
+
+        public FormularioBasicoModel()
+        {
+            listNombres = new List<string>();
+            listNombres.Add("Anton Vol");
+            listNombres.Add("Jesuita Gol");
+            listNombres.Add("Golina Patrioski");
+            listNombres.Add("Pat Gamorra");
+            listNombres.Add("Hem Helsing");
+        }
+
 
     }
 }
